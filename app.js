0e25e83aece2a3833e53b6712b94d7aa54a60592
Leandro Gentili <leandrogentili@gmail.com>
2016-05-12 11:57:15 +0200

    maybe it was fixed

--- app.js
+++ app.js
@@ -1,44 +1,52 @@
-var app = angular.module('testApp', []);
+var app = angular.module('testApp', [])
 
 app.controller('FormulaController', function ($scope) {
-    $scope.input = {
-        a1: "", b1: "", c1: "", 
-        a2: "", b2: "", c2: "", 
-        a3: "", b3: "", c3: "", 
-    };
-});
+  $scope.input = {
+    a1: '', b1: '', c1: '',
+    a2: '', b2: '', c2: '',
+    a3: '', b3: '', c3: '',
+  }
+})
 
+app.directive('cell', function ($timeout, $parse) {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    scope: {
+      cellName: '@',
+      cellObserves: '=',
+      cellFormula: '&cellFormula',
+      ngModel: '='
+    },
+    require: 'ngModel',
+    link: function ($scope, element, $attrs, ngModelCtrl) {
+      var name = $scope.cellName
+      var observes = $scope.cellObserves
+      var formula = $scope.cellFormula
 
-app.directive('cell', function ($timeout) {
-    return {
-        restrict: 'A',
-        scope: {
-            cellName:     '@',
-            cellObserves: '=',
-            cellFormula:  '&cellFormula',
-        },
-        require: 'ngModel',
-        link: function ($scope, element, $attrs, ngModel) {
-            var name     = $scope.cellName;
-            var observes = $scope.cellObserves;
-            var formula  = $scope.cellFormula;
+      element.bind('change', function () {
+        $scope.$parent.$broadcast(name + 'Changed')
+      })
 
-            element.bind('change', function () {
-                $scope.$parent.$broadcast(name+"Changed");
-            });
+      if (observes && formula) {
+        angular.forEach(observes, function (o) {
+          $scope.$on(o + 'Changed', function (event, data) {
+            $timeout(function () {
+              var result = formula()
 
-            if (observes && formula) {
-                angular.forEach(observes, function (o) {
-                    $scope.$on(o+"Changed", function (event, data) {
-                        $timeout(function () {
-                            // qui ci va qualcosa tipo ngModel.$viewModel = formula();
-                            console.log(formula());
+              // Update view
+              ngModelCtrl.$viewValue = result
+              ngModelCtrl.$render()
 
-                            $scope.$parent.$broadcast(name+"Changed");
-                        });
-                    });
-                })
-            }
-        }
+              // Update model
+              ngModelCtrl.$modelValue = result
+              $scope.ngModel = result
+
+              $scope.$parent.$broadcast(name + 'Changed')
+            })
+          })
+        })
+      }
     }
-});
+  }
+})
